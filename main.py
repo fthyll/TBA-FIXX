c89b07e30d5d7ae90ce37c4e402b5aebe19b3146
Muhammad Fatih Yumna Lajuwirdi Lirrahman <m.fatihyumna25@gmail.com>
2023-06-18 20:57:36 +0700

    main init

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from parser_1 import parse
-from lex_analyzer import lexAnalyze
+from parser_1 import parse # import fungsi parse dari parser_1.py
+from lex_analyzer import lexAnalyze # import fungsi lexAnalyze dari lex_analyzer.py
 
-def main():
-    tokens = []
-    with open('input.txt') as f:
-        lines = f.readlines()
-        lines = ''.join(lines)
-        tokens = parse(lines)
+def main(): 
+    tokens = [] # daftar token yang sudah diurutkan
+    with open('input.txt') as f: # membaca file input.txt
+        lines = f.readlines() # membaca setiap baris
+        lines = ''.join(lines) # menggabungkan setiap baris
+        tokens = parse(lines) # memanggil fungsi parse
      
-    print("\n".join(tokens[0]))
-    lexAnalyze(tokens[0])
+    print("\n".join(tokens[0])) # menampilkan token yang sudah diurutkan
+    lexAnalyze(tokens[0]) # memanggil fungsi lexAnalyze
 
 if __name__ == "__main__":
     main()
